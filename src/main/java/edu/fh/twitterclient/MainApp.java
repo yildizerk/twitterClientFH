278136a30bee2366cd57909dc90524006581c251
lennart-WOB <>
2014-10-20 15:56:15 +0200

    Login.fxml - button align right

--- src/main/java/edu/fh/twitterclient/MainApp.java
+++ src/main/java/edu/fh/twitterclient/MainApp.java
@@ -1,5 +1,7 @@
 package edu.fh.twitterclient;
 
+import edu.fh.twitterclient.controller.LoginController;
+import java.net.URL;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
@@ -7,28 +9,37 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
+/**
+ * MainApp class
+ *
+ * 
+ * @author lennart-WOB
+ * @version 1.0
+ * 
+ */
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        final URL fxmlLocation = getClass().getResource("/fxml/Login.fxml");
+        final FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
+        fxmlLoader.setController(new LoginController());
+        final Parent root = fxmlLoader.load();
         
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
+        scene.getStylesheets().add("/styles/Login.css");
         
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("Twitter Client based on JavaFX");
         stage.setScene(scene);
         stage.show();
     }
 
     /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
+     * main() Methode wird ignoriert, da es sich um eine JavaFX Anwendung handelt.
+     * Nur für den Fall, dass unerwartete Fehler auftreten und das Programm
+     * nicht startet.
      *
-     * @param args the command line arguments
+     * @param args Kommandozeilen Parameter
      */
     public static void main(String[] args) {
         launch(args);
